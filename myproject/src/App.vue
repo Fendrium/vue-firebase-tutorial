a9ec2a840b0f0d4e6a089998617eb3e1ffbbb4e1
Charlie <charlie.nguyenvanphuc@gmail.com>
2018-10-17 00:51:41 +0200

    Initialized tutorial project

---
+++ myproject/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h1> {{ title }} </h1>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'My first Vue App'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+  text-align: center;
+}
+
+
+</style>
